d2534768468070ed54ff5951188a3770246ca933
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2025-10-17 16:18:35 +0700

    update enum DatabaseType.java

--- src/main/java/mino/dx/curseletcraft/database/DatabaseType.java
+++ src/main/java/mino/dx/curseletcraft/database/DatabaseType.java
@@ -1,6 +1,47 @@
 package mino.dx.curseletcraft.database;
 
+@SuppressWarnings("unused")
 public enum DatabaseType {
-    MYSQL,
-    SQLITE
+    MYSQL(new TypeString("mysql", false)),
+    SQLITE(new TypeString("sqlite", false)),
+    MYSQL_ASYNC(new TypeString("mysql", true)),
+    SQLITE_ASYNC(new TypeString("sqlite", true));
+
+    private record TypeString(String databaseType, boolean isAsync) {}
+    private final TypeString typeString;
+
+    DatabaseType(TypeString typeString) {
+        this.typeString = typeString;
+    }
+
+    public String getName() {
+        return typeString.databaseType();
+    }
+
+    public boolean isAsync() {
+        return typeString.isAsync();
+    }
+
+    public boolean isMySQL() {
+        return typeString.databaseType.equalsIgnoreCase("mysql");
+    }
+
+    /**
+     * Parse enum dựa trên 2 biến boolean trong config
+     * @param isAsync true nếu bật chế độ bất đồng bộ
+     * @param isMySQL true nếu sử dụng MySQL, false nếu SQLite
+     * @return DatabaseType tương ứng
+     */
+    public static DatabaseType parseType(boolean isAsync, boolean isMySQL) {
+        if (isAsync) {
+            return isMySQL ? MYSQL_ASYNC : SQLITE_ASYNC;
+        } else {
+            return isMySQL ? MYSQL : SQLITE;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return (isAsync() ? "Async " : "Sync ") + getName().toUpperCase();
+    }
 }
